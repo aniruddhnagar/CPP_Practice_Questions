4445b2a07cb5fdb60c48573debb40287088473f1
ruddh <aniruddhnagar@gmail.com>
2021-12-27 09:47:07 +0530

    coding concepts

---
+++ 17a_digital_root_using_recursion.cpp
@@ -0,0 +1,55 @@
+/*
+You are given a number n. 
+You need to find the digital root of n. 
+DigitalRoot of a number is the recursive sum of its digits 
+until we get a single digit number.
+
+Example:
+Input:
+n = 99999
+Output: 9
+Explanation: Sum of digits of 99999 is 45
+which is not a single digit number, hence
+sum of digit of 45 is 9 which is a single
+digit number.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int sumDigits(int);
+int digitalRoot(int);
+
+int main() {
+
+    int n;
+    cout<< endl <<"Enter number: ";
+    cin>> n;
+    
+    cout<< endl <<digitalRoot(n);
+
+	return 0;
+}
+
+// function to calculate digit sum of a number
+int sumDigits(int n)
+{
+    // returning n if n / 10 is 0
+    if(n / 10 == 0)
+        return n;
+    // else adding digit sum
+    else
+        return n % 10 + sumDigits(n/10);
+}
+
+
+// function to calculate the single digit answer
+int digitalRoot(int n)
+{
+    // if n is less than n then, returning the n
+    if(n / 10 == 0)
+        return n;
+    // else calling sumDigits function
+    else
+        return digitalRoot(sumDigits(n));   
+}
